a6b4360ef648159170d3e8aa20a693597fddc18f
rob <robertchen234@gmail.com>
2019-05-06 22:49:47 -0400

    passed second challenge

---
+++ notes-app/app.js
@@ -0,0 +1,18 @@
+// const fs = require("fs");
+
+// fs.writeFileSync("notes.txt", "My name is Rob.");
+// fs.appendFileSync("notes.txt", " Hello.");
+
+// const add = require("./utils.js");
+
+// const sum = add(4, -2);
+
+// console.log(sum);
+
+const fs = require("fs");
+
+// fs.writeFileSync('notes.js', 'Hello')
+
+const note = require("./notes.js");
+
+console.log(note);
